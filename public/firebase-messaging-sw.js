c77225a1419346b870f98894651bbe051bed9294
Febri Milansyah <febrimilan00@gmail.com>
2023-01-02 17:56:31 +0700

    Add FCM Service Workers

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+// This a service worker file for receiving push notifitications.
+// See `Access registration token section` @ https://firebase.google.com/docs/cloud-messaging/js/client#retrieve-the-current-registration-token
+
+// Scripts for firebase and firebase messaging
+importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging.js')
+
+// Initialize the Firebase app in the service worker by passing the generated config
+firebase.initializeApp({
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+})
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging()
+
+// Handle incoming messages while the app is not in focus (i.e in the background, hidden behind other tabs, or completely closed).
+messaging.onBackgroundMessage(function (payload) {
+  console.log('Received background message ', payload)
+
+  const notificationTitle = payload.notification.title
+  const notificationOptions = {
+    body: payload.notification.body,
+  }
+
+  self.registration.showNotification(notificationTitle, notificationOptions)
+})
